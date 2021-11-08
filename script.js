c655a83af38bd8496c133b4f8eefff10162bf433
Dmitriy <plotnikovdi95@gmail.com>
2021-11-08 10:15:34 +0300

    Сделана основа

---
+++ script.js
@@ -0,0 +1,35 @@
+const textarea1 = document.querySelector('.content .textarea1')
+const textarea2 = document.querySelector('.content .textarea2')
+
+const resultOne = document.querySelector('.result-one span')
+const resultTwo = document.querySelector('.result-two span')
+
+let res1 = ''
+let res2 = ''
+
+textarea1.addEventListener('input', function () {
+	let text = this.value
+	res1 = text.trim().length
+	res2 = text.split(' ').join('').split('\n').join('').trim().length
+	textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
+	resultOne.innerHTML = res1
+	resultTwo.innerHTML = res2
+})
+
+const addData = document.querySelector('.addData')
+const table = document.querySelector('.table table')
+addData.addEventListener('click', function () {
+	const tr = document.createElement('tr')
+	const td1 = document.createElement('td')
+	const td2 = document.createElement('td')
+	const td3 = document.createElement('td')
+
+	td1.textContent = 1
+	td2.textContent = res1
+	td3.textContent = res2
+
+	tr.appendChild(td1)
+	tr.appendChild(td2)
+	tr.appendChild(td3)
+	table.appendChild(tr)
+})
