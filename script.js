89d1b567660ed8b3b21f5271de630cc34495f72d
Dmitriy <plotnikovdi95@gmail.com>
2021-11-08 16:45:51 +0300

    Кол-во символов теперь добавляется в буфер обмена

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const resultTwo = document.querySelector('.result-two span')
 let res1 = ''
 let res2 = ''
 
+const modal = document.querySelector('.modal')
+
 textarea1.addEventListener('input', function () {
 	let text = this.value
 	res1 = text.trim().length
@@ -14,6 +16,19 @@ textarea1.addEventListener('input', function () {
 	textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
 	resultOne.innerHTML = res1
 	resultTwo.innerHTML = res2
+
+	// копирование текста в буфер обмена
+	const input = document.createElement('input')
+	document.querySelector('body').append(input)
+	input.value = res1
+	input.select()    
+	document.execCommand("copy")
+	input.remove()
+
+	modal.classList.add('animation')
+	setTimeout(function() {
+		modal.classList.remove('animation')
+	}, 1500)
 })
 
 const addData = document.querySelector('.addData')
@@ -32,4 +47,6 @@ addData.addEventListener('click', function () {
 	tr.appendChild(td2)
 	tr.appendChild(td3)
 	table.appendChild(tr)
+
+
 })
