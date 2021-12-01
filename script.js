daa31ff6d4a82eaae31ff1dcb91d23c1f6291329
Dmitriy <plotnikovdi95@gmail.com>
2021-12-01 16:19:30 +0300

    Работа с буфером обмена теперь реализована через  navigator.clipboard

--- script.js
+++ script.js
@@ -10,43 +10,141 @@ let res2 = ''
 const modal = document.querySelector('.modal')
 
 textarea1.addEventListener('input', function () {
-	let text = this.value
-	res1 = text.trim().length
-	res2 = text.split(' ').join('').split('\n').join('').trim().length
-	textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
-	resultOne.innerHTML = res1
-	resultTwo.innerHTML = res2
+  // let text = this.value
 
-	// копирование текста в буфер обмена
-	const input = document.createElement('input')
-	document.querySelector('body').append(input)
-	input.value = res1
-	input.select()    
-	document.execCommand("copy")
-	input.remove()
+  // numChar = text.trim().length
+  // numCharWithoutSpaces = text
+  //   .split(' ')
+  //   .join('')
+  //   .split('\n')
+  //   .join('')
+  //   .trim().length
 
-	modal.classList.add('animation')
-	setTimeout(function() {
-		modal.classList.remove('animation')
-	}, 1500)
+  // textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
+  // resultOne.innerHTML = numChar
+  // resultTwo.innerHTML = numCharWithoutSpaces
+
+  let [numChar, numCharWithoutSpaces] = countChar()
+
+  // копирование текста в буфер обмена
+  //   const input = document.createElement('input')
+  //   document.querySelector('body').append(input)
+  //   input.value = res1
+
+  //   input.select()
+  //   document.execCommand('copy')
+
+  let resultClipboard = ''
+  navigator.clipboard
+    .writeText(numChar)
+    .then(() => {
+      resultClipboard = `В буфер обмена скопировано кол-во симвлов: ${numChar}`
+    })
+    .catch(err => {
+      resultClipboard = `Что-то пошло не так. Ошибка: ${err}`
+    })
+    .finally(() => {
+      showModal(resultClipboard)
+      navigator.clipboard.writeText(numChar)
+    })
+
+  //   input.remove()
 })
 
-const addData = document.querySelector('.addData')
-const table = document.querySelector('.table table')
-addData.addEventListener('click', function () {
-	const tr = document.createElement('tr')
-	const td1 = document.createElement('td')
-	const td2 = document.createElement('td')
-	const td3 = document.createElement('td')
+// const addData = document.querySelector('.addData')
+// const table = document.querySelector('.table table')
+// addData.addEventListener('click', function () {
+//   const tr = document.createElement('tr')
+//   const td1 = document.createElement('td')
+//   const td2 = document.createElement('td')
+//   const td3 = document.createElement('td')
 
-	td1.textContent = 1
-	td2.textContent = res1
-	td3.textContent = res2
+//   td1.textContent = 1
+//   td2.textContent = res1
+//   td3.textContent = res2
 
-	tr.appendChild(td1)
-	tr.appendChild(td2)
-	tr.appendChild(td3)
-	table.appendChild(tr)
+//   tr.appendChild(td1)
+//   tr.appendChild(td2)
+//   tr.appendChild(td3)
+//   table.appendChild(tr)
+// })
 
+const сlipboardPastCopy = document.querySelector('.сlipboardPastCopy')
 
+сlipboardPastCopy.addEventListener('click', () => {
+  ;(async function main() {
+    await clipboardRead()
+    let result = await clipboardWrite()
+    await showModal(result)
+  })()
 })
+
+const сlipboardPast = document.querySelector('.сlipboardPast')
+сlipboardPast.addEventListener('click', () => {
+  clipboardRead().then(res => showModal(res))
+})
+
+const сlipboardCopy = document.querySelector('.сlipboardCopy')
+сlipboardCopy.addEventListener('click', () => {
+  clipboardWrite().then(res => showModal(res))
+})
+
+function showModal(content) {
+  modal.innerHTML = `<p>${content}</p>`
+  modal.classList.add('animation')
+  setTimeout(function () {
+    modal.classList.remove('animation')
+  }, 1500)
+}
+
+// Подсчет символов
+function countChar() {
+  let text = textarea1.value
+  let numChar = text.trim().length
+  let numCharWithoutSpaces = text
+    .split(' ')
+    .join('')
+    .split('\n')
+    .join('')
+    .trim().length
+  textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
+  resultOne.innerHTML = numChar
+  resultTwo.innerHTML = numCharWithoutSpaces
+
+  return [numChar, numCharWithoutSpaces]
+}
+
+// Вставка из буфера обмена
+function clipboardRead() {
+  return new Promise(function (resolve, reject) {
+    navigator.clipboard
+      .readText()
+      .then(text => {
+        result = 'Текст успешно вставлен'
+        textarea1.value = text
+        resolve(result)
+      })
+      .catch(err => {
+        result = `Что-то пошло не так. Ошибка: ${err}`
+        reject(result)
+      })
+  })
+}
+
+// Копирование в буфер обмена
+function clipboardWrite() {
+  return new Promise(function (resolve, reject) {
+    let [numChar] = countChar()
+    navigator.clipboard
+      .writeText(numChar)
+      .then(() => {
+        resultClipboard = `В буфер обмена скопировано кол-во симвлов: ${numChar}`
+        navigator.clipboard.writeText(numChar)
+        resolve(resultClipboard)
+      })
+      .catch(err => {
+        resultClipboard = `Что-то пошло не так. Ошибка: ${err}`
+        reject(resultClipboard)
+      })
+  })
+}
