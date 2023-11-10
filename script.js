09d60e9290b3ea3290137b39c89f6276b5fef630
Dmitriy <plotnikovdi95@gmail.com>
2023-11-10 14:37:04 +0300

    добавлен файл readme

--- script.js
+++ script.js
@@ -12,29 +12,7 @@ let res2 = ''
 const modal = document.querySelector('.modal')
 
 textarea1.addEventListener('input', function () {
-  // let text = this.value
-
-  // numChar = text.trim().length
-  // numCharWithoutSpaces = text
-  //   .split(' ')
-  //   .join('')
-  //   .split('\n')
-  //   .join('')
-  //   .trim().length
-
-  // textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
-  // resultOne.innerHTML = numChar
-  // resultTwo.innerHTML = numCharWithoutSpaces
-
   let [numChar, numCharWithoutSpaces] = countChar()
-
-  // копирование текста в буфер обмена
-  //   const input = document.createElement('input')
-  //   document.querySelector('body').append(input)
-  //   input.value = res1
-
-  //   input.select()
-  //   document.execCommand('copy')
 
   let resultClipboard = ''
   navigator.clipboard
@@ -49,27 +27,7 @@ textarea1.addEventListener('input', function () {
       showModal(resultClipboard)
       navigator.clipboard.writeText(numChar)
     })
-
-  //   input.remove()
 })
-
-// const addData = document.querySelector('.addData')
-// const table = document.querySelector('.table table')
-// addData.addEventListener('click', function () {
-//   const tr = document.createElement('tr')
-//   const td1 = document.createElement('td')
-//   const td2 = document.createElement('td')
-//   const td3 = document.createElement('td')
-
-//   td1.textContent = 1
-//   td2.textContent = res1
-//   td3.textContent = res2
-
-//   tr.appendChild(td1)
-//   tr.appendChild(td2)
-//   tr.appendChild(td3)
-//   table.appendChild(tr)
-// })
 
 const сlipboardPastCopy = document.querySelector('.сlipboardPastCopy')
 
@@ -103,12 +61,7 @@ function showModal(content) {
 function countChar() {
   let text = textarea1.value
   let numChar = text.trim().length
-  let numCharWithoutSpaces = text
-    .split(' ')
-    .join('')
-    .split('\n')
-    .join('')
-    .trim().length
+  let numCharWithoutSpaces = text.split(' ').join('').split('\n').join('').trim().length
   textarea2.innerHTML = text.split(' ').join('').split('\n').join('')
   resultOne.innerHTML = numChar
   resultTwo.innerHTML = numCharWithoutSpaces
@@ -155,13 +108,9 @@ const btnShow = document.querySelector('#btn-show')
 const textResult = document.querySelector('.text-result')
 
 btnShow.addEventListener('click', function () {
-  textResult.style.display =
-    textResult.style.display == 'block' ? 'none' : 'block'
+  textResult.style.display = textResult.style.display == 'block' ? 'none' : 'block'
 
-  this.innerHTML =
-    textResult.style.display == 'block'
-      ? 'Скрыть получившийся текст'
-      : 'Показать получившийся текст'
+  this.innerHTML = textResult.style.display == 'block' ? 'Скрыть получившийся текст' : 'Показать получившийся текст'
 })
 
 // Проверка того что находится в буфере обмена
